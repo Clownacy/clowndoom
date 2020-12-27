51c45cf9d43e66689750693cdd9c5e18d65af2c4
Clownacy <Clownacy@users.noreply.github.com>
2020-12-27 20:24:46 +0000

    Get sndserv to compile
    OSS isn't a thing anymore, so this will need modifying

--- sndserv/linux.c
+++ sndserv/linux.c
@@ -38,6 +38,7 @@ static const char rcsid[] = "$Id: linux.c,v 1.3 1997/01/26 07:45:01 b1 Exp $";
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
 
