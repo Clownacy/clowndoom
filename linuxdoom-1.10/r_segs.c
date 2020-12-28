92b6eda7618cfc4ff5aa711e6d05766e93224c41
Clownacy <Clownacy@users.noreply.github.com>
2020-12-28 16:40:01 +0000

    "Fix" some invalid memory accesses
    I'm sure I'm just treating symptoms of other bugs, rather than the
    actual cause.

--- linuxdoom-1.10/r_segs.c
+++ linuxdoom-1.10/r_segs.c
@@ -257,7 +257,7 @@ void R_RenderSegLoop (void)
 	{
 	    // calculate texture offset
 	    angle = (rw_centerangle + xtoviewangle[rw_x])>>ANGLETOFINESHIFT;
-	    texturecolumn = rw_offset-FixedMul(finetangent[angle],rw_distance);
+	    texturecolumn = rw_offset-FixedMul(finetangent[angle&(FINEANGLES/2-1)],rw_distance);
 	    texturecolumn >>= FRACBITS;
 	    // calculate lighting
 	    index = rw_scale>>LIGHTSCALESHIFT;
